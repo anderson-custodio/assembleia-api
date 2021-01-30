61febeedfef972490cce81740b03f161164de1bd
Anderson Fernando Custódio <afcustodioo@gmail.com>
2021-01-30 11:50:30 -0300

    Cria projeto Spring Boot usando Initializr.

---
+++ src/main/java/br/com/sicredi/assembleia/AssembleiaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.assembleia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssembleiaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssembleiaApiApplication.class, args);
+	}
+
+}
