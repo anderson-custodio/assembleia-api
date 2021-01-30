06e69ba027925f6eceec0717809aef7141b77020
Anderson Fernando Custódio <afcustodioo@gmail.com>
2021-01-30 14:31:03 -0300

    Adiciona repositories para as entidades.

---
+++ src/main/java/br/com/sicredi/assembleia/repository/PautaRepository.java
@@ -0,0 +1,9 @@
+package br.com.sicredi.assembleia.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.sicredi.assembleia.domain.Pauta;
+
+public interface PautaRepository extends JpaRepository<Pauta, Long> {
+
+}
---
+++ src/main/java/br/com/sicredi/assembleia/repository/VotoRepository.java
@@ -0,0 +1,9 @@
+package br.com.sicredi.assembleia.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.sicredi.assembleia.domain.Voto;
+
+public interface VotoRepository extends JpaRepository<Voto, Long> {
+
+}
